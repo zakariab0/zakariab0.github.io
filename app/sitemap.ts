2026f3567a7f15499fff0151bf28ced51d53c0cc
Zakaria Bounou <z.bounou@lavoieexpress.ma>
2025-11-17 17:41:14 +0100

    last update

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,7 @@
 import { getBlogPosts } from "app/blog/utils";
 
-export const baseUrl = "https://zakariabounou.vercel.app";
+export const baseUrl = "https://zakariab0.github.io";
+export const dynamic = 'force-static';
 
 export default async function sitemap() {
   let blogs = getBlogPosts().map((post) => ({
